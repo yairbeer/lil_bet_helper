ac3312b92137aa19c3ce22f8a7e9b64bb6a1b26b
yair <yaia1309@gmail.com>
2016-01-10 10:35:27 +0200

    reading files

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ print games_stats
 
 print ('Calculate Results')
 games_goals = games_stats.GoalsA.values + games_stats.GoalsB.values
-print games_goals[:20]
 games_goals_score = (games_goals <= 2) * 1 + (games_goals > 3) * 3
 games_goals_score = (games_goals_score == 0) * 2 + (games_goals_score != 0) * games_goals_score
-print games_goals_score
 games_win_score = (games_stats.GoalsA.values > games_stats.GoalsB.values) * 1 + \
                   (games_stats.GoalsA.values < games_stats.GoalsB.values) * 2
-print games_win_score[:20]
 games_win_score_A_adv = ((games_stats.GoalsA.values + 1) > games_stats.GoalsB.values) * 1 + \
                         ((games_stats.GoalsA.values + 1) < games_stats.GoalsB.values) * 2
-games_win_score = (games_stats.GoalsA.values > (games_stats.GoalsB.values + 1)) * 1 + \
-                  (games_stats.GoalsA.values < (games_stats.GoalsB.values + 1)) * 2
+games_win_score_B_adv = (games_stats.GoalsA.values > (games_stats.GoalsB.values + 1)) * 1 + \
+                        (games_stats.GoalsA.values < (games_stats.GoalsB.values + 1)) * 2
