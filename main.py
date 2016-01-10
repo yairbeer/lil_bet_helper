cdfaa92aba12fce03a0e16fa3de4d2c7115dd76d
yair <yaia1309@gmail.com>
2016-01-10 10:31:39 +0200

    reading files

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ games_stats = pd.read_csv('20082015games.csv')
 print games_stats
 
 print ('Calculate Results')
+games_goals = games_stats.GoalsA.values + games_stats.GoalsB.values
+print games_goals[:20]
+games_goals_score = (games_goals <= 2) * 1 + (games_goals > 3) * 3
+games_goals_score = (games_goals_score == 0) * 2 + (games_goals_score != 0) * games_goals_score
+print games_goals_score
+games_win_score = (games_stats.GoalsA.values > games_stats.GoalsB.values) * 1 + \
+                  (games_stats.GoalsA.values < games_stats.GoalsB.values) * 2
+print games_win_score[:20]
